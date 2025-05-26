9fc7269af07dc31b5fc8dbc5fa86139b399d5c39
Trofim Fedoseev <fedseevtaf@yandex.ru>
2025-05-26 15:50:20 +0300

    Add implementation of the x87_top

---
+++ src/nastx.asm
@@ -0,0 +1,155 @@
+global x87_top, x87_head, x87_stack
+
+global x87_gist, x87_ngist
+global x87_about, x87_nabout
+
+global x87_inform, x87_ninform
+global x87_inspect, x87_ninspect
+
+extern printf
+
+
+%macro MAKE_OFFSET_TAB_ENTRY 4
+%1%4:	equ (%2 + %4 * %3)
+%endmacro
+
+%macro MAKE_OFFSET_TAB 4
+%assign i 0
+%rep %4
+	MAKE_OFFSET_TAB_ENTRY %1, %2, %3, i
+%assign i i+1
+%endrep
+%endmacro
+
+	struc SAVE
+.CW	equ 0
+.SW	equ 4
+.TW	equ 8
+.FIP	equ 12
+.FDP	equ 20
+.R	equ 28
+MAKE_OFFSET_TAB .R, .R, 10, 8
+	endstruc
+
+	struc X87DUMP
+.CW	equ 0
+.SW	equ 4
+.TW	equ 8
+.R	equ 12
+MAKE_OFFSET_TAB .R, .R, 12, 8
+.FIP	equ 108
+.FDP	equ 116
+	endstruc
+
+%macro F_TWORD_MEMCPY_ZX 2
+	mov eax, dword [%2]
+	mov dword [%1], eax
+	mov eax, dword [%2 + 4]
+	mov dword [%1 + 4], eax
+	movzx eax, word [%2 + 8]
+	mov dword [%1 + 8], eax
+%endmacro
+
+%macro F_TWORD_MEMCPY_ZX_TAB2TAB 6
+	F_TWORD_MEMCPY_ZX %1 + %2%3, %4 + %5%6
+%endmacro
+
+%macro F_EXPAND_SAVE_TO_X87DUMP 1
+	mov eax, dword [%1 + SAVE.FIP]
+	mov dword [%1 + X87DUMP.FIP], eax
+	mov eax, dword [%1 + SAVE.FIP + 4]
+	mov dword [%1 + X87DUMP.FIP + 4], eax
+
+	mov eax, dword [%1 + SAVE.FDP]
+	mov dword [%1 + X87DUMP.FDP], eax
+	mov eax, dword [%1 + SAVE.FDP + 4]
+	mov dword [%1 + X87DUMP.FDP + 4], eax
+
+%assign i 0
+%rep
+	F_TWORD_MEMCPY_ZX_TAB2TAB %1, SAVE.R, i, %2, X87DUMP.R, i
+%assign i i+1
+%endrep
+%endmacro
+
+%macro F_ENTER_DUMP 0-1
+	push ebp
+	mov ebp, esp
+	push esi
+	push edi
+	sub esp, 128
+%if %0 > 0
+	%1 [esp+4]
+%else
+	fsave [esp+4]
+%endif
+	F_EXPAND_SAVE_TO_X87DUMP esp+4
+%endmacro
+
+%macro F_LEAVE_DUMP
+	mov edi, dword [ebp-8]
+	mov esi, dword [ebp-4]
+	leave
+%endmacro
+
+
+section .rodata
+fmt_stack_register_value:	db "|ST(%u)|%22.8Lg|", 10, 0
+fmt_stack_register_empty:	db "|ST(%u)|                   ...|", 10, 0
+
+fmt:				db "%x%x%x", 10, 0
+
+section .text
+x87_ntop:
+	F_ENTER_DUMP fnsave
+	jmp .body
+x87_top:
+	F_ENTER_DUMP
+.body:
+
+	movzx cx, word [esp+4+DUMP.SW]
+	movzx ax, word [esp+4+DUMP.TW]
+
+	shl cx, 10
+	and cl, 14
+	shr ax, cl
+	and al, 3
+	cmp al, 3
+	jz .print_empty
+.print_value:
+	shr ecx, 1
+	lea ecx, [ecx*2 + ecx]
+	F_TWORD_MEMCPY_ZX esp+8, esp+4 + (X87DUMP.R + ecx)
+	mov dword [esp+4], 0
+	mov dword [esp], fmt_stack_register_value
+	call printf
+
+	F_LEAVE_DUMP
+	ret
+
+.print_empty:
+	mov dword [esp+4], 0
+	mov dword [esp], fmt_stack_register_empty
+	call printf
+
+	F_LEAVE_DUMP
+	ret
+
+x87_head:
+x87_nhead:
+x87_stack:
+x87_nstack:
+x87_gist:
+x87_ngist:
+x87_about:
+x87_nabout:
+x87_inform:
+x87_ninform:
+x87_inspect:
+x87_ninspect:
+	ret
+
+
+
+
+
---
+++ src/nastx.h
@@ -0,0 +1,43 @@
+#ifndef NASTX87_H_SENTRY
+#define NASTX87_H_SENTRY
+
+/*
+ * nastx87 can be used as a C library, all the functions do use cdecl
+ */
+
+void
+x87_top(void);
+
+void
+x87_head(void);
+
+void
+x87_stack(void);
+
+void
+x87_gist(void);
+
+void
+x87_ngist(void);
+
+void
+x87_about(void);
+
+void
+x87_nabout(void);
+
+void
+x87_inform(void);
+
+void
+x87_ninform(void);
+
+void
+x87_inspect(void);
+
+void
+x87_ninspect(void);
+
+#endif /* NASTX87_H_SENTRY */
+
+
